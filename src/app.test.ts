a37c34db0ac5410936e053d3dfde18bc3b0ec22e
hisomura <hisomura.noreply@gmail.com>
2021-04-04 16:54:05 +0900

    feat: Sequelizeを動作させてテストするところの手前まで実装

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,5 +1,7 @@
 import request from "supertest";
 import { app, server } from "./app";
+// @ts-ignore
+import { sequelize, User } from "./models";
 
 describe("first express test", () => {
   test("/ return 'Hello, World!'", async () => {
@@ -8,7 +10,13 @@ describe("first express test", () => {
     expect(response.text).toBe("Hello, World!");
   });
 
-  afterAll(() => {
+  test("/ return 'Hello, World!'", async () => {
+    const response = await request(app).get("/users");
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBe(1);
+  });
+
+  afterAll(async () => {
     server.close();
   });
 });
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express from "express";
+// @ts-ignore
+// import User from "./models/user.js";
+import { User } from "./models";
 
 export const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
+});
+
+app.get("/users", async (req, res) => {
+  const users = await User.findAll();
+  res.send(users);
 });
 
 export const server = app.listen(port, () => {
