c1b91e0f00f31c9223a481d3fcc80740932e52b5
hisomura <hisomura.noreply@gmail.com>
2021-04-07 07:50:40 +0900

    feat: プログラム内でマイグレーションを実行し、factory-girlでデータ作成

--- src/factory_girl_sample.js
+++ src/factory_girl_sample.js
@@ -1,14 +1,49 @@
 const FactoryGirl = require("factory-girl");
 const factory = FactoryGirl.factory;
 const adapter = new FactoryGirl.SequelizeAdapter();
-const User = require("./models").User;
+const models = require("./models");
+const User = models.User;
 
-factory.setAdapter(adapter)
-factory.define("user", User, {
-  firstName: "Bob",
-  lastName: "Tanaka",
+const Sequelize = require("sequelize");
+const path = require("path");
+const Umzug = require("umzug");
+
+// creates a basic sqlite database
+// const sequelize = new Sequelize({ dialect: "sqlite", storage: ":memory:" });
+const sequelize = User.sequelize
+
+const umzug = new Umzug({
+  migrations: {
+    // indicates the folder containing the migration .js files
+    path: path.join(__dirname, "/../db/migrations"),
+    // inject sequelize's QueryInterface in the migrations
+    params: [
+      sequelize.getQueryInterface(),
+      Sequelize
+    ],
+  },
+
+  // indicates that the migration data should be store in the database
+  // itself through sequelize. The default configuration creates a table
+  // named `SequelizeMeta`.
+  storage: "sequelize",
+  storageOptions: {
+    sequelize: sequelize,
+  },
+  logging: console.log,
 });
 
-factory.build("user").then((user) => {
-  console.log(user.firstName, user.lastName); // => User {username: 'Bob', score: 50}
+(async () => {
+  // checks migrations and run them if they are not already applied
+  await umzug.up();
+  console.log("All migrations performed successfully");
+})().then(async () => {
+  factory.setAdapter(adapter);
+  factory.define("user", User, {
+    firstName: "Bob",
+    lastName: "Tanaka",
+  });
+
+  await factory.create("user");
+  console.log(await User.findAll());
 });
