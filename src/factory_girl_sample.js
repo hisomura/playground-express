5cba1fe52c4608fa1f1e110e685629757034c074
hisomura <hisomura.noreply@gmail.com>
2021-04-08 21:03:48 +0900

    feat: Postデータ生成まで。

--- src/factory_girl_sample.js
+++ src/factory_girl_sample.js
@@ -3,6 +3,8 @@ const factory = FactoryGirl.factory;
 const adapter = new FactoryGirl.SequelizeAdapter();
 const models = require("./models");
 const User = models.User;
+const Post = models.Post;
+const faker = require("faker");
 
 const Sequelize = require("sequelize");
 const path = require("path");
@@ -10,17 +12,14 @@ const Umzug = require("umzug");
 
 // creates a basic sqlite database
 // const sequelize = new Sequelize({ dialect: "sqlite", storage: ":memory:" });
-const sequelize = User.sequelize
+const sequelize = User.sequelize;
 
 const umzug = new Umzug({
   migrations: {
     // indicates the folder containing the migration .js files
     path: path.join(__dirname, "/../db/migrations"),
     // inject sequelize's QueryInterface in the migrations
-    params: [
-      sequelize.getQueryInterface(),
-      Sequelize
-    ],
+    params: [sequelize.getQueryInterface(), Sequelize],
   },
 
   // indicates that the migration data should be store in the database
@@ -40,10 +39,21 @@ const umzug = new Umzug({
 })().then(async () => {
   factory.setAdapter(adapter);
   factory.define("user", User, {
-    firstName: "Bob",
-    lastName: "Tanaka",
+    firstName: faker.name.firstName,
+    lastName: faker.name.lastName,
+    email: faker.internet.email,
+  });
+
+  factory.define("post", Post, {
+    userId: 1,
+    title: faker.name.title,
+    body: faker.lorem.paragraph,
   });
 
   await factory.create("user");
+  const user = await User.findOne();
+  const post = await factory.create("post", { userId: user.id });
+  console.log(user, post);
   console.log(await User.findAll());
+  console.log(await Post.findAll());
 });
--- src/models/post.js
+++ src/models/post.js
@@ -3,23 +3,23 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class Post extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      this.posts = this.hasMany(models.Post)
+      this.user = this.belongsTo(models.User)
     }
   };
-  User.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING
+  Post.init({
+    userId: DataTypes.INTEGER,
+    title: DataTypes.STRING,
+    body: DataTypes.STRING,
   }, {
     sequelize,
-    modelName: 'User',
+    modelName: 'Post',
   });
-  return User;
+  return Post;
 };
