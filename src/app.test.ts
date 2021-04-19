2158d4149aea3319a0526a0370daa52b9fb37187
hisomura <hisomura.noreply@gmail.com>
2021-04-20 07:13:59 +0900

    feat: デフォルトでmysqlでテストを実行するように変更。

--- src/app.test.ts
+++ src/app.test.ts
@@ -19,7 +19,10 @@ describe("first express test", () => {
   beforeAll(async () => {
     // await migrate();
     // マイグレーション直接実行するより速い
-    await sequelize.sync();
+    if ("mysql" === sequelize.getDialect()) {
+      await sequelize.query('SET GLOBAL FOREIGN_KEY_CHECKS = 0')
+    }
+    await sequelize.sync({ force: true });
     await setupFactories();
   });
   afterEach(async () => {
@@ -38,13 +41,6 @@ describe("first express test", () => {
   });
 
   describe("/users", () => {
-    it("GETリクエストで全てのユーザーを返す", async () => {
-      const users = await factory.createMany(User.name, 2);
-      const response = await client.get("/users");
-      expect(response.data.length).toBe(2);
-      expect(response.data).toEqual(JSON.parse(JSON.stringify(users)));
-    });
-
     it("GETリクエストで指定したソート順でユーザーを返す", async () => {
       await factory.create<User>(User.name, { firstName: "Taro" });
       await factory.create<User>(User.name, { firstName: "Ichiro" });
